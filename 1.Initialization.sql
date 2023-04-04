33341567f7745b85ca10ca906ee0b1bd70cdd740
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-04-04 13:04:00 +0530

    Refactor into 3 files

--- 1.Initialization.sql
+++ 1.Initialization.sql
@@ -1,48 +1,48 @@
 --- CREATING THE TABLES
-CREATE TABLE Property (
-    propertyID INT NOT NULL PRIMARY KEY,
-    ownerID VARCHAR(12) NOT NULL,
-    rentPerMonth INT,
-    availableFrom DATE,
-    availableTill DATE,
-    totalArea INT NOT NULL,
-    plinthArea INT NOT NULL,
-    noOfFloors INT,
-    yearOfConstruction INT NOT NULL,
-    locality VARCHAR(100),
-    address VARCHAR(500),
-    annualHike INT,
-    residential INT DEFAULT 0,
-    noOfBedrooms INT    
+CREATE TABLE PROPERTY (
+    PROPERTYID INT NOT NULL PRIMARY KEY,
+    OWNERID VARCHAR(12) NOT NULL,
+    RENTPERMONTH INT,
+    AVAILABLEFROM DATE,
+    AVAILABLETILL DATE,
+    TOTALAREA INT NOT NULL,
+    PLINTHAREA INT NOT NULL,
+    NOOFFLOORS INT,
+    YEAROFCONSTRUCTION INT NOT NULL,
+    LOCALITY VARCHAR(100),
+    ADDRESS VARCHAR(500),
+    ANNUALHIKE INT,
+    RESIDENTIAL INT DEFAULT 0,
+    NOOFBEDROOMS INT
 );
 
-CREATE TABLE Users (
-    aadharID VARCHAR(12) PRIMARY KEY NOT NULL,
-    loginID VARCHAR(15) NOT NULL,
-    loginPassword VARCHAR(15) NOT NULL,
-    name VARCHAR(30),
-    age INT,
-    doorNo INT,
-    pinCode INT,
-    street VARCHAR(50),
-    isManager INT DEFAULT 0,
-    isDBA INT DEFAULT 0
+CREATE TABLE USERS (
+    AADHARID VARCHAR(12) PRIMARY KEY NOT NULL,
+    LOGINID VARCHAR(15) NOT NULL,
+    LOGINPASSWORD VARCHAR(15) NOT NULL,
+    NAME VARCHAR(30),
+    AGE INT,
+    DOORNO INT,
+    PINCODE INT,
+    STREET VARCHAR(50),
+    ISMANAGER INT DEFAULT 0,
+    ISDBA INT DEFAULT 0
 );
 
-CREATE TABLE UsersPhoneNumbers (
-    aadharID VARCHAR(12) NOT NULL,
-    phoneNumber INT,
-    CHECK(phoneNumber > 1000000000 AND phoneNumber <= 9999999999)
+CREATE TABLE USERSPHONENUMBERS (
+    AADHARID VARCHAR(12) NOT NULL,
+    PHONENUMBER INT,
+    CHECK(PHONENUMBER > 1000000000 AND PHONENUMBER <= 9999999999)
 );
 
-CREATE TABLE Tenantship (
-    rentedPropertyID INT NOT NULL,
-    startDate DATE NOT NULL,
-    endDate Date NOT NULL,
-    tenantID VARCHAR(12) NOT NULL,
-    rentPerMonth INT,
-    yearlyHike INT,
-    agencyCommission INT
+CREATE TABLE TENANTSHIP (
+    RENTEDPROPERTYID INT NOT NULL,
+    STARTDATE DATE NOT NULL,
+    ENDDATE DATE NOT NULL,
+    TENANTID VARCHAR(12) NOT NULL,
+    RENTPERMONTH INT,
+    YEARLYHIKE INT,
+    AGENCYCOMMISSION INT
 );
 
 --- ADDING FAKE DATA
@@ -75,112 +75,12 @@ INSERT INTO Tenantship VALUES (5,2023-07-01,2024-06-30,100010003003,45000,4,1000
 
 
 --- ADDING KEYS
-ALTER TABLE Tenantship ADD CONSTRAINT TenantshipPropertyFK FOREIGN KEY (rentedPropertyID) REFERENCES Property(propertyID);
-ALTER TABLE Tenantship ADD CONSTRAINT TenantshipPropertyUserFK FOREIGN KEY (tenantID) REFERENCES Users(aadharID);
-ALTER TABLE Property ADD CONSTRAINT PropertyUsersFK FOREIGN KEY (ownerID) REFERENCES Users(aadharID);
-ALTER TABLE UsersPhoneNumbers ADD CONSTRAINT UsersPhoneNumbersUsersFK FOREIGN KEY (aadharID) REFERENCES Users(aadharID);
-ALTER TABLE UsersPhoneNumbers ADD CONSTRAINT UsersPhoneNumbersPK PRIMARY KEY (aadharID, phoneNumber);
+ALTER TABLE TENANTSHIP ADD CONSTRAINT TENANTSHIPPROPERTYFK FOREIGN KEY (RENTEDPROPERTYID) REFERENCES PROPERTY(PROPERTYID);
 
---- Procedure for adding a new user
-CREATE OR REPLACE PROCEDURE createNewUser(aadharID IN varchar2, password IN varchar2, name IN varchar2, age IN number, doorNo IN number,  pinCode IN number, street IN varchar2) AS
-BEGIN
-INSERT INTO Users VALUES (aadharID, password, name, age, doorNo, pinCode, street, 0, 0);
-END;
-/
+ALTER TABLE TENANTSHIP ADD CONSTRAINT TENANTSHIPPROPERTYUSERFK FOREIGN KEY (TENANTID) REFERENCES USERS(AADHARID);
 
-CREATE OR REPLACE PROCEDURE addPhoneNumber(aadharID IN varchar2, phoneNumber IN varchar2) AS
-BEGIN
-INSERT INTO UsersPhoneNumbers VALUES (aadharID, phoneNumber);
-END;
-/
----Generic Procedures
----Adding data to the users table
-CREATE OR REPLACE PROCEDURE Insert_Property (
-    p_propertyID IN Property.propertyID%TYPE,
-    p_ownerID IN Property.ownerID%TYPE,
-    p_rentPerMonth IN Property.rentPerMonth%TYPE,
-    p_availableFrom IN Property.availableFrom%TYPE,
-    p_availableTill IN Property.availableTill%TYPE,
-    p_totalArea IN Property.totalArea%TYPE,
-    p_plinthArea IN Property.plinthArea%TYPE,
-    p_noOfFloors IN Property.noOfFloors%TYPE,
-    p_yearOfConstruction IN Property.yearOfConstruction%TYPE,
-    p_locality IN Property.locality%TYPE,
-    p_address IN Property.address%TYPE,
-    p_annualHike IN Property.annualHike%TYPE,
-    p_residential IN Property.residential%TYPE,
-    p_noOfBedrooms IN Property.noOfBedrooms%TYPE
-) AS
-BEGIN
-    INSERT INTO Property (
-        propertyID,
-        ownerID,
-        rentPerMonth,
-        availableFrom,
-        availableTill,
-        totalArea,
-        plinthArea,
-        noOfFloors,
-        yearOfConstruction,
-        locality,
-        address,
-        annualHike,
-        residential,
-        noOfBedrooms
-    ) VALUES (
-        p_propertyID,
-        p_ownerID,
-        p_rentPerMonth,
-        p_availableFrom,
-        p_availableTill,
-        p_totalArea,
-        p_plinthArea,
-        p_noOfFloors,
-        p_yearOfConstruction,
-        p_locality,
-        p_address,
-        p_annualHike,
-        p_residential,
-        p_noOfBedrooms
-    );
-    COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        ROLLBACK;
-        DBMS_OUTPUT.PUT_LINE('Error inserting data into Property table: ' || SQLERRM);
-END Insert_Property;
-/
+ALTER TABLE PROPERTY ADD CONSTRAINT PROPERTYUSERSFK FOREIGN KEY (OWNERID) REFERENCES USERS(AADHARID);
 
----CREATING USERS
----DBA
-CREATE USER dba_user IDENTIFIED BY dba_user;
-GRANT CREATE SESSION, CREATE PROCEDURE TO dba_user;
+ALTER TABLE USERSPHONENUMBERS ADD CONSTRAINT USERSPHONENUMBERSUSERSFK FOREIGN KEY (AADHARID) REFERENCES USERS(AADHARID);
 
----Adding user to the database by DBA with necessary privileges.
-CREATE OR REPLACE PROCEDURE add_user(
-    p_aadhar_id IN Users.aadharID%TYPE,
-    p_login_id IN Users.loginID%TYPE,
-    p_login_password IN Users.loginPassword%TYPE,
-    p_name IN Users.name%TYPE,
-    p_age IN Users.age%TYPE,
-    p_door_no IN Users.doorNo%TYPE,
-    p_pin_code IN Users.pinCode%TYPE,
-    p_street IN Users.street%TYPE,
-    p_is_manager IN Users.isManager%TYPE DEFAULT 0,
-    p_is_dba IN Users.isDBA%TYPE DEFAULT 0
-)
-AUTHID CURRENT_USER
-IS
-BEGIN
-    -- check if current user has necessary privileges
-    IF NOT (USER = 'DBA' AND p_is_dba = 1) THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Only a DBA can execute this procedure.');
-    END IF;
-    
-    -- insert user data into Users table
-    INSERT INTO Users(aadharID, loginID, loginPassword, name, age, doorNo, pinCode, street, isManager, isDBA)
-    VALUES(p_aadhar_id, p_login_id, p_login_password, p_name, p_age, p_door_no, p_pin_code, p_street, p_is_manager, p_is_dba);
-END;
-/
-
-
+ALTER TABLE USERSPHONENUMBERS ADD CONSTRAINT USERSPHONENUMBERSPK PRIMARY KEY (AADHARID, PHONENUMBER);
